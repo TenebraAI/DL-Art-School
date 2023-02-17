a09cf98c7fed711e75e9d87d4fc63192eeaf2ee9
mrq <barry.quiggles@protonmail.com>
2023-02-17 15:47:55 +0000

    more cleanup, pip-ifying won't work, got an alternative

--- setup.py
+++ setup.py
@@ -18,7 +18,56 @@ setuptools.setup(
     project_urls={},
     scripts=[],
     include_package_data=True,
-    install_requires=parse_requirements('requirements.txt', session='hack'),
+    install_requires=[
+        # Fundamentals
+        "numpy",
+        "pyyaml",
+        "tb-nightly",
+        "future",
+        "scp",
+        "tqdm",
+        "matplotlib",
+        "scipy",
+        "munch",
+        "tqdm",
+        "scp",
+        "tensorboard",
+        "orjson",
+        "einops",
+        "lambda-networks",
+        "mup",
+
+        # For image generation stuff
+        "opencv-python",
+        "kornia",
+        "pytorch_ssim",
+        "gsa-pytorch",
+        "pytorch_fid",
+
+        # For audio generation stuff
+        "inflect",
+        "librosa",
+        "Unidecode",
+        "tgt",
+        "pyworld",
+        "audio2numpy",
+        "SoundFile",
+
+        # For text stuff
+        "transformers",
+        "tokenizers",
+        "jiwer",  # calculating WER
+        "omegaconf",
+
+        # lucidrains stuff
+        "vector_quantize_pytorch",
+        "linear_attention_transformer",
+        "rotary-embedding-torch",
+        "axial_positional_embedding",
+        "g-mlp-pytorch",
+        "x-clip",
+        "x_transformers==1.0.4",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
